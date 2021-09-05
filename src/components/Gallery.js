e7245cfeee6657c40ce20884a623d971dfd347f2
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-05 17:40:15 +0200

    Splitting more components and apart and using props

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,7 +8,8 @@ const Gallery = (props) => {
 <div class="photo-container">
         <h2>Results</h2>
         <ul>
-          <Picture />
+          <Picture
+          url={"https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"} />
           <NotFound />
         </ul>
       </div>
