e24ccb5ffd37c9491f4102c48bb57581a1384eb5
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-05 18:14:37 +0200

    Attempting to get search result from flickr and map it

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import './css/index.css';
 import './Config'
 import { BrowserRouter, Route} from 'react-router-dom'
 import axios from 'axios';
-import React from 'react'
-
+import React, { Component } from 'react'
+import apiKey from './Config';
 import Search from './components/Search';
 import Navigation from './components/Navigation';
 import Gallery from './components/Gallery';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
+class App extends Component{
+state = {
+  photos: []
+}
+
+// Search call 
+// Using https://www.flickr.com/services/api/explore/flickr.photos.search
+Search = text => axios.get(`www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=cat&format=json&nojsoncallback=1`)
+.then(response => {
+  this.SetState({ photos: response.data.photos.photos})
+});
+
+ render(){ return (
     <div class="container">
       <Search />
       <Navigation />
-      <Gallery />
+      <Gallery
+      photos={this.state.photos} />
     </div>
   );
 }
+}
+
 
 export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,24 @@
 // Imports
-import React from 'react';
+import React, { Component, PureComponent } from 'react';
+
+import { withRouter } from 'react-router-dom'
 import NotFound from './NotFound'
 import Picture from './Picture'
 // Code 
-const Gallery = (props) => {
+class Gallery extends PureComponent {
+render(){
   return (
 <div class="photo-container">
         <h2>Results</h2>
         <ul>
-          <Picture
-          url={"https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"} />
+        this.props.photos;.map(result => 
+          <Picture url={`https://farm${result.farm}.staticflickr.com/${result.server}/${result.id}_${result.secret}_n.jpg`} />);
+          {/* <Picture
+          url={"https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"} /> */}
           <NotFound />
         </ul>
       </div>
-  )
+  )}
 }
 
 export default Gallery
