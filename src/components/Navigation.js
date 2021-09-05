bf924ed6fe9ddad7b8580a34491780b8c077d9fa
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-05 09:44:59 +0200

    Splitted navigation into seperate component from index.html

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+// Imports
+import React from 'react';
+
+// Code
+const Navigation = () => {
+    return (
+        <nav class="main-nav">
+        <ul>
+          <li><a href='#'>Cats</a></li>
+          <li><a href='#'>Dogs</a></li>
+          <li><a href='#'>Computers</a></li>
+        </ul>
+      </nav>
+    )
+  }
+  
+  export default Navigation
