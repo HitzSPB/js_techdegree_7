42bb2987f15699ed4f95685e3e51ad8da81b8465
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-05 17:35:29 +0200

    Splitted it into more components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,13 +5,13 @@ import { NavLink } from 'react-router-dom'
 // Code
 const Navigation = () => {
     return (
-        <nav className="main-nav">
+        <nav class="main-nav">
         <ul>
-          <li><NavLink to='/cats'>Cats</NavLink></li>
-          <li><NavLink to='/dogs'>Dogs</NavLink></li>
-          <li><NavLink to='/rabbit'>Rabbits</NavLink></li>
-        </ul >
-      </nav >
+          <li><a href='#'>Cats</a></li>
+          <li><a href='#'>Dogs</a></li>
+          <li><a href='#'>Computers</a></li>
+        </ul>
+      </nav>
     )
   }
   
