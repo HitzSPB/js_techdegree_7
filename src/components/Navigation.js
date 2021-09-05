c2b7e0d46144b501a8819f91d3aa70baad64b9a3
HitzSPB <daniel.oesterbro@gmail.com>
2021-09-05 17:24:56 +0200

    Added navlink to navigation - but getting error on startup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,17 @@
 // Imports
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 // Code
 const Navigation = () => {
     return (
-        <nav class="main-nav">
+        <nav className="main-nav">
         <ul>
-          <li><a href='#'>Cats</a></li>
-          <li><a href='#'>Dogs</a></li>
-          <li><a href='#'>Computers</a></li>
-        </ul>
-      </nav>
+          <li><NavLink to='/cats'>Cats</NavLink></li>
+          <li><NavLink to='/dogs'>Dogs</NavLink></li>
+          <li><NavLink to='/rabbit'>Rabbits</NavLink></li>
+        </ul >
+      </nav >
     )
   }
   
